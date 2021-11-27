53041a9508c733c338069fe07fdc29515fea2330
Adam Hannigan <adam@payble.com.au>
2021-11-28 09:50:20 +1100

    [FEAT] Add pancake swap URLs

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -106,16 +106,17 @@ export default function Example() {
           <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
             <div className="mx-auto max-w-7xl lg:px-8">
               <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center z-10">
                   <div className="lg:py-24">
                     <a
-                      href="#"
+                      target="_blank"
+                      href="https://pancakeswap.finance/swap?outputCurrency=0x06872971938462D0b206D01fe98eCc3E73798b89"
                       className="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
                     >
                       <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-teal-500 to-cyan-600 rounded-full">
                         Pancake Swap
                       </span>
-                      <span className="ml-4 text-sm">Coming soon...</span>
+                      <span className="ml-4 text-sm">Buy now</span>
                       <ChevronRightIcon
                         className="ml-2 w-5 h-5 text-gray-500"
                         aria-hidden="true"
@@ -143,12 +144,14 @@ export default function Example() {
                       >
                         <div className="sm:flex">
                           <div className="mt-3 sm:mt-0">
-                            <button
+                            <a
                               type="submit"
+                              href="https://pancakeswap.finance/swap?outputCurrency=0x06872971938462D0b206D01fe98eCc3E73798b89"
+                              target="_blank"
                               className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
                             >
                               Buy Now
-                            </button>
+                            </a>
                           </div>
                         </div>
                         <p className="mt-3 text-sm text-gray-300 sm:mt-4">
