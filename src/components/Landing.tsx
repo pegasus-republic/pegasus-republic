e0f66bcaddd852dca6c0c522aa79f3c0d31f7668
Adam Hannigan <adam@payble.com.au>
2021-12-08 18:19:54 +1100

    [FEAT] Add liquidity banner

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -23,8 +23,46 @@ const navigation = [
 ];
 
 export default function Example() {
+  const [showBanner, setShowBanner] = React.useState(true);
+
   return (
     <div className="bg-white">
+      {showBanner && (
+        <div className="relative bg-cyan-600">
+          <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+            <div className="pr-16 sm:text-center sm:px-16">
+              <p className="font-medium text-white">
+                <span className="md:hidden">
+                  The community has just burned 65% of liquidity.
+                </span>
+                <span className="hidden md:inline">
+                  The community has just burned 65% of liquidity.
+                </span>
+                <span className="block sm:ml-2 sm:inline-block">
+                  <a
+                    href="https://t.me/+L2bZSku7KAs4NTQ1"
+                    className="text-white font-bold underline"
+                  >
+                    {" "}
+                    Join us on Telegram<span aria-hidden="true">&rarr;</span>
+                  </a>
+                </span>
+              </p>
+            </div>
+            <div className="absolute inset-y-0 right-0 pt-1 pr-1 flex items-start sm:pt-1 sm:pr-2 sm:items-start">
+              <button
+                type="button"
+                onClick={() => setShowBanner(false)}
+                className="flex p-2 rounded-md hover:bg-cyan-500 focus:outline-none focus:ring-2 focus:ring-white"
+              >
+                <span className="sr-only">Dismiss</span>
+                <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="relative overflow-hidden">
         <Popover as="header" className="relative">
           <div className="bg-gray-900 pt-6">
