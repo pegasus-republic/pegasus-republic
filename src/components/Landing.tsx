9a8b7e9f65558685aefca12575898ef01a3a0d33
Adam Hannigan <adam@payble.com.au>
2022-01-30 08:27:47 -0300

    [FIX] JPG build issue

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -196,7 +196,7 @@ export default function Example() {
                               }}
                               className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
                             >
-                              Buy Now
+                              Pancake Swap
                             </button>
                           </div>
                         </div>
